f3173742599f63230280a40ffdcb1a8a67aff68f
Konstantin <pro100kot14@gmail.com>
2019-09-23 10:23:00 +0300

    add preprocessor example

---
+++ interestingness/preprocessor.c
@@ -0,0 +1,15 @@
+// Use gcc -E to see more
+#include <stdio.h>
+#define N 10
+#define M N + N
+#define ABS(x) -x
+#define HELLO "Hola amigos"
+#define LETSGO main()
+#define BACKBACKBACK return
+
+int LETSGO{
+
+	printf(HELLO": %d\n", -N + ABS(M - N));
+
+	BACKBACKBACK 0;
+}
