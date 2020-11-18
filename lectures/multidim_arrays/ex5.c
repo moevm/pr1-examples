46979f1e4379a9250bea8bda640c4cd8045c2d7e
Gosudarkin Yaroslav <gosudarkin.yaroslav@gmail.com>
2020-11-18 09:33:55 +0300

    ex5

--- lectures/multidim_arrays/ex5.c
+++ lectures/multidim_arrays/ex5.c
@@ -6,23 +6,22 @@
 // how many letters 1 block wil contain
 #define BUFF_SZ 100
 
-// this ex. doesn't working natively
+// how many BLOCKS text contains
+#define TEXT_BLOCKS 10
 
 int main()
 {
     // allocate memory for 1 block
-    char* text=calloc(BUFF_SZ,sizeof(char));
+    char** text=calloc(TEXT_BLOCKS,sizeof(char*));
     char c;
 
     // main loop, each time geting letter from stdin. You can use another loop break condition instead "\n", EOF for e.g
     while((c=getchar())!='\n')
     {
-        // here you need to fill up "text" string by letters and extend "text" 
+        // here you need to fill up "text" by blocks of chars that you read from stdin and extend "text" 
 
         // do the work
     }
 
-    printf("[%s]", text);
-    
     return 0;
 }
