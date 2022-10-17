7098522ac7685f2d7618e9006b153cf9c625f554
Konstantin Chaika <pro100kot14@gmail.com>
2022-10-17 11:56:39 +0400

    example of reading unlim str, lecture 17 october 2022

---
+++ lectures/17oct22_read_unlim_str/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int x;
+#define N 10
+
+int* initIntArray(int n){
+//	int *arr = (int*)malloc(sizeof(int)*n);
+	int *arr = calloc(n, sizeof(int));
+	for(int i=0; i<n; i++){
+		arr[i] = i;
+	}
+	return arr;
+}
+
+int main(){
+	int cap = 5;
+	char *s = malloc(cap);
+	char c;
+	int i = 0;
+	while((c = getchar()) != '\n'){
+		printf("processing %c on %d iteration\n", c, i);
+		if(i == (cap-1)){
+			printf("realloc %d -> %d\n", cap, cap+5);
+			char *t = realloc(s, cap+5);
+			if(t){
+				s = t;
+				cap += 5;
+			}else {
+				// error handler
+			}
+		}
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	puts(s);
+
+
+	int *p = initIntArray(30);
+	int *temp = realloc(p, sizeof(int)*60);
+	if(temp){ // temp != NULL
+		p = temp;
+	} else {
+		//???
+	}
+
+
+
+	free(p);
+	
+
+
+	return 0;
+}
